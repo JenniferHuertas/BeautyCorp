af9a0af631ab9bf05a5f1b24d0f2f3d3f91c3a23
jennlindley <jennifersu2059@gmail.com>
2020-04-21 12:49:09 -0500

    deploy

---
+++ precache-manifest.a33dc304fe7e876fda107887efad9f0d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec97a0bb4ab81fd063c",
+    "url": "/BeautyCorp/css/app.78f87aaa.css"
+  },
+  {
+    "revision": "15f5b9118efef27fcc8a",
+    "url": "/BeautyCorp/css/chunk-vendors.4e21e629.css"
+  },
+  {
+    "revision": "a8c76eeef5ac071b7138",
+    "url": "/BeautyCorp/css/home.e22872a2.css"
+  },
+  {
+    "revision": "181be3f47209194606dc",
+    "url": "/BeautyCorp/css/login.03af9c2b.css"
+  },
+  {
+    "revision": "34f92579cec4d54d9984",
+    "url": "/BeautyCorp/css/login~register~reset.d4ed6e64.css"
+  },
+  {
+    "revision": "9c5e0787bb12859f314d",
+    "url": "/BeautyCorp/css/products.2bf78d08.css"
+  },
+  {
+    "revision": "d571182c765275af1e18",
+    "url": "/BeautyCorp/css/register.0fc35619.css"
+  },
+  {
+    "revision": "333bda8b1e224e1bcaa8",
+    "url": "/BeautyCorp/css/reset.516d7c17.css"
+  },
+  {
+    "revision": "ca901fed24b08f22760fbf2e904f0f1f",
+    "url": "/BeautyCorp/img/base studio obscure.ca901fed.jpg"
+  },
+  {
+    "revision": "6c6c43ebe5d7e6acc665923bd6ee14df",
+    "url": "/BeautyCorp/img/byb.6c6c43eb.jpg"
+  },
+  {
+    "revision": "8f38c2f83a5b5efca469b5d43e9989b7",
+    "url": "/BeautyCorp/img/catalogo esika.8f38c2f8.jpg"
+  },
+  {
+    "revision": "754a00033e06509eafb26f6bbfd287a6",
+    "url": "/BeautyCorp/img/catalogo foto.754a0003.jpg"
+  },
+  {
+    "revision": "ad0c5ecc7c6dc442b8ad9efe333ec753",
+    "url": "/BeautyCorp/img/cliente.ad0c5ecc.svg"
+  },
+  {
+    "revision": "b4100668b9cb1f7a1fb4a59ee3071cf0",
+    "url": "/BeautyCorp/img/esika-logo.b4100668.svg"
+  },
+  {
+    "revision": "2c53fa1aeda0130aa62287476e18ba32",
+    "url": "/BeautyCorp/img/logo.2c53fa1a.png"
+  },
+  {
+    "revision": "6169362e41771143f3cc64cd6030f170",
+    "url": "/BeautyCorp/img/star.6169362e.svg"
+  },
+  {
+    "revision": "e34a28add2ed7e5930d9ec7080789bd8",
+    "url": "/BeautyCorp/index.html"
+  },
+  {
+    "revision": "aec97a0bb4ab81fd063c",
+    "url": "/BeautyCorp/js/app.1c4b75eb.js"
+  },
+  {
+    "revision": "15f5b9118efef27fcc8a",
+    "url": "/BeautyCorp/js/chunk-vendors.aae7364f.js"
+  },
+  {
+    "revision": "a8c76eeef5ac071b7138",
+    "url": "/BeautyCorp/js/home.56b84191.js"
+  },
+  {
+    "revision": "181be3f47209194606dc",
+    "url": "/BeautyCorp/js/login.f04cebae.js"
+  },
+  {
+    "revision": "34f92579cec4d54d9984",
+    "url": "/BeautyCorp/js/login~register~reset.27a14c19.js"
+  },
+  {
+    "revision": "9c5e0787bb12859f314d",
+    "url": "/BeautyCorp/js/products.c8ae0a83.js"
+  },
+  {
+    "revision": "d571182c765275af1e18",
+    "url": "/BeautyCorp/js/register.c2a825be.js"
+  },
+  {
+    "revision": "333bda8b1e224e1bcaa8",
+    "url": "/BeautyCorp/js/reset.aea14ff4.js"
+  },
+  {
+    "revision": "21817bb9a5d3bac27f296b53420ceec5",
+    "url": "/BeautyCorp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BeautyCorp/robots.txt"
+  }
+]);
